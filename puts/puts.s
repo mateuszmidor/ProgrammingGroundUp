2dced4f564bfcc8e4263446e0f1ef7e82b5b0726
Mateusz Midor <mateusz>
2017-01-26 12:06:44 +0100

    Made linker flags specific to each example, added puts example

---
+++ puts/puts.s
@@ -0,0 +1,14 @@
+.section .data
+msg:
+.ascii "Halo!\0"
+
+.section .text
+.global _start
+
+_start:
+    movl $msg, %edi # edi = &msg[0]
+    call puts       # puts(msg)
+    movl $0, %ebx   # 0 as return code
+    int $0x80       # exit(0)
+
+
